29c8309586d1c0e14f656f2f787365f853857cb4
Angeles Feuillade <angife8@hotmail.com>
2025-02-26 16:19:30 -0300

    Cambio en el codigo de barra de prosucto, ahora lo guarda especifico a un producto, solo se actualiza al crear un nuevo producto

---
+++ FarmaciaData/src/test/java/com/example/FarmaciaData/FarmaciaDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FarmaciaData;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmaciaDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
